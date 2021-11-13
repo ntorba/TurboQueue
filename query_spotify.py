739931799fb4a3422f60af0d99faf9efe36154fc
ntorba <32570754+ntorba@users.noreply.github.com>
2021-11-12 22:55:16 -0500

    Create query_spotify.py

---
+++ query_spotify.py
@@ -0,0 +1,29 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def mac_miller_songs(n=5):
+    mac_miller_uri = "spotify:artist:4LLpKhyESsyAXpc4laK94U"
+    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    results = spotify.artist_top_tracks(mac_miller_uri)
+    # for track in results["tracks"][:10]:
+    #     print("track    : " + track["name"])
+    #     print("audio    : " + track["preview_url"])
+    #     print("cover art: " + track["album"]["images"][0]["url"])
+    #     print()
+
+    tracks = results["tracks"][:n]
+    table_info = []
+    for i_track in tracks:
+        track_info = {}
+        track_info["artist"] = i_track["artists"][0]["name"]
+        track_info["name"] = i_track["name"]
+        track_info["uri"] = i_track["uri"]
+        track_info["img_sm"] = i_track["album"]["images"][-1]["url"]
+        track_info["img_md"] = i_track["album"]["images"][1]["url"]
+        track_info["img_lg"] = i_track["album"]["images"][0]["url"]
+        table_info.append(track_info)
+    return table_info
